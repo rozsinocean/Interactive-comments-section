c8118d3f0f6d9bf7c19f8a8f362319e068561c7c
Rosana <rosana.escola.pro@gmail.com>
2023-02-07 21:34:25 +0000

    Different push

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:rozsinocean/News-HomePage.git main:gh-pages
+git push -f git@github.com:rozsinocean/Interactive-comments-section main:gh-pages
 
 cd -
